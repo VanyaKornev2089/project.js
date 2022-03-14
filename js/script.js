5038ded4193179e004d6ed3ac0e2aa956f13ca64
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-03-14 14:33:27 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "myProject";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = "4856258";
+let rollback = Math.random() * 50;
+let fullPrice = 175000;
+let adaptive = true;
+let uTT = screens.split('')
 
-alert("Hello guys!");
+console.log ('title' + ' ' + title);
+console.log ('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей');
+console.log ('adaptive' + ' ' + adaptive);
+console.log ('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей');
+console.log (screens.length);
+console.log (screens.toLowerCase);
+console.log (uTT);
+console.log ('Процент отката посреднику за работу' + ' ' + Math.round(fullPrice * (rollback/100)));
 
-console.log("I love you");
 
+
+console.log (typeof title);
+console.log (typeof fullPrice);
+console.log (typeof adaptive);
+console.log (typeof screenPrice);
+console.log (typeof  rollback);

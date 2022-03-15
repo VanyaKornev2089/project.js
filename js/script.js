db36d9070e17c922880c7fb10ce27101be78d609
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-03-15 11:44:14 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-let title = "myProject";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = "4856258";
+let title = prompt("Как называется ваш проект?", "myProject");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
 let rollback = Math.random() * 50;
-let fullPrice = 175000;
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let uTT = screens.split('')
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?", "service1")
+let servicePrice1 = +prompt("Сколько это будет стоить?", "5000")
+let service2 = prompt("Какой дополнительный тип услуги нужен?", "service2")
+let servicePrice2 = +prompt("Сколько это будет стоить?", "5000")
+let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+let  servicePercentPrice = Math.ceil(fullPrice + rollback / 100);
+
+if (fullPrice > 30000){
+console.log ('Даём скидку в 10%')
+}
+else if ((fullPrice > 15000) && (fullPrice < 30000)){
+    console.log ('Даём скидку в 5%')
+}
+else if ((fullPrice < 15000) && (fullPrice > 0)){
+    console.log ('Скидка не предусмотрена')
+}
+else{
+    console.log ('Что то пошло не так')
+}
+    
+
+
+
+
 
 console.log ('title' + ' ' + title);
 console.log ('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей');
 console.log ('adaptive' + ' ' + adaptive);
 console.log ('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей');
 console.log (screens.length);
-console.log (screens.toLowerCase);
-console.log (uTT);
+console.log (screens.toLowerCase().split(', '));
+
+
 console.log ('Процент отката посреднику за работу' + ' ' + Math.round(fullPrice * (rollback/100)));
 
 

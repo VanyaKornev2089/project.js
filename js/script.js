caa9057e181c1fe248d57d270b1ad72078087187
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-03-18 13:40:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,41 +9,84 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = +prompt("Сколько это будет стоить?", "5000")
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "service2")
 let servicePrice2 = +prompt("Сколько это будет стоить?", "5000")
+let allServicePrices;
 let fullPrice = (screenPrice + servicePrice1 + servicePrice2);
-let  servicePercentPrice = Math.ceil(fullPrice + rollback / 100);
+let servicePercentPrice;
 
-if (fullPrice > 30000){
-console.log ('Даём скидку в 10%')
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
 }
-else if ((fullPrice > 15000) && (fullPrice < 30000)){
-    console.log ('Даём скидку в 5%')
-}
-else if ((fullPrice < 15000) && (fullPrice > 0)){
-    console.log ('Скидка не предусмотрена')
-}
-else{
-    console.log ('Что то пошло не так')
-}
+
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        console.log(price);
+        return 'Даём скидку в 10%'
+    } else if (15000 <= price && price < 30000) {
+        console.log(price);
+        return 'Даем скидку в 5%'
+    } else if (0 <= price && price < 15000){
+        console.log(price);
+        return 'Скидка не предусмотрена'
+    } else {
+        console.log(price);
+        return 'Что-то пошло не так'
+    }
+};
+
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+};
+allServicePrices = getAllServicePrices();
+
+const getFullPrice = function() {
+    return screenPrice + allServicePrices;
+};
+fullPrice =  getFullPrice();
+
+const getTitle = function() {
+    let titleNew = title.split('')[0].toUpperCase()
+    let title2 = title.slice(1);
+    title = titleNew + title2
+    return title
+}; 
+title = getTitle(); 
+
+const  getServicePercentPrices = function() {
+    return fullPrice - (fullPrice * (rollback / 100));
+};
+servicePercentPrice = getServicePercentPrices();
     
 
 
 
 
 
-console.log ('title' + ' ' + title);
-console.log ('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей');
-console.log ('adaptive' + ' ' + adaptive);
-console.log ('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей');
-console.log (screens.length);
-console.log (screens.toLowerCase().split(', '));
 
-
-console.log ('Процент отката посреднику за работу' + ' ' + Math.round(fullPrice * (rollback/100)));
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
 
 
 
-console.log (typeof title);
-console.log (typeof fullPrice);
-console.log (typeof adaptive);
-console.log (typeof screenPrice);
-console.log (typeof  rollback);
+    
+console.log(getRollbackMessage(fullPrice));
+
+
+
+// console.log ('title' + ' ' + title);
+// console.log ('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей');
+// console.log ('adaptive' + ' ' + adaptive);
+// console.log ('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей');
+// console.log (screens.length);
+// console.log (screens.toLowerCase().split(', '));
+
+
+// console.log ('Процент отката посреднику за работу' + ' ' + Math.round(fullPrice * (rollback/100)));
+
+
+
+// console.log (typeof title);
+// console.log (typeof fullPrice);
+// console.log (typeof adaptive);
+// console.log (typeof screenPrice);
+// console.log (typeof  rollback);
